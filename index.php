054c78a036d6a979927c4bd2f20556033def7226
Mohammed Khubaib <102320167+Mohammed-Khubaib@users.noreply.github.com>
2023-01-01 20:16:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,5 @@ $name = getenv('NAME' ,true) ?: 'GDSC MCET';
 echo sprintf('%s test page',$name);
 
 echo "<h3>This is just to check if the cloud run and cloud SQL are connecting properly : </h3> <br> change the link to test.php";
+echo "<h4>checking if the CI/CD is working properly</h4>";
 ?>
