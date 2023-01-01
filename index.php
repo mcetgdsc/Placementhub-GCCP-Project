73b34525a23594f161be8b42f712a0c90cd5f38a
Mohammed Khubaib <102320167+Mohammed-Khubaib@users.noreply.github.com>
2023-01-01 21:04:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ echo sprintf('%s test page',$name);
 
 echo "<h3>This is just to check if the cloud run and cloud SQL are connecting properly : </h3> <br> change the link to test.php";
 echo "<h4>checking if the CI/CD is working properly</h4>";
+echo "<h4>is CI/CD is working !</h4>";
 ?>
