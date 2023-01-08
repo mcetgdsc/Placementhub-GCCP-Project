46fb8db0eb2889c36ec5e36d058e7af80f57ea97
Khubaib <mohmmedkhubaib2001@gmail.com>
2023-01-08 16:54:45 +0530

    Trying to connect to the cloud SQL Database

--- dbconnect.php
+++ dbconnect.php
@@ -1,15 +1,20 @@
 <?php
-
 $SERVER = "34.100.218.188";
-$username = "placementhub";
-$password = "1234";
-$database = "internship";
+$username = 'placementhub';
+$password = '1234';
+$db_name = 'internship';
+$cloud_sql_connection_name = getenv("CLOUD_SQL_CONNECTION_NAME");
+$socket_dir = getenv('DB_SOCKET_DIR')?: '/cloudsql';
+
+$dsn = sprintf(
+'mysql:dbname=%s;unix_socket=%s/%s',
+$db_name,
+$socket_dir,
+$cloud_sql_connection_name
+);
 $conn = mysqli_connect($SERVER, $username, $password, $database);
 if (!$conn) {
     mysqli_error($conn);
 }
 // echo "connect successfull";
-
-
-
 ?>
